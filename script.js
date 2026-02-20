950b4a16466c5e66b0c55a4c6a1d7c25545dfeee
Taaccii <tom1991@hotmail.it>
2026-02-20 18:23:30 +0100

    feat: add advancecd logic
    add logic for backspace button
    add logic for clear button
    add logic for double operator input problem
    add logic for repeat operation with equal button
    add logic for upper-display

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function divide(a, b) {
 let firstNumber = null;
 let operator = null;
 let secondNumber = null;
-
+let calculated = false;
 
 function operate(operator, firstNumber, secondNumber) {
   switch (operator) {
@@ -28,8 +28,73 @@ function operate(operator, firstNumber, secondNumber) {
     case '/':
       return divide(firstNumber, secondNumber);
     default:
-      return 'Invalid operator';
+      return ;
   }
 }
 
+function fontSizeSet() {
+  if (mainDisplay.textContent.length > 10) {
+    mainDisplay.style.fontSize = '24px';
+  } else {
+    mainDisplay.style.fontSize = '40px';
+  }
+}
 
+const buttons = document.querySelectorAll('button');
+const mainDisplay = document.querySelector('.main-display');
+const upperDisplay = document.querySelector('.upper-display')
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (mainDisplay.textContent.length >= 18) return;
+
+    if (button.classList.contains('number')) {
+      const digit = button.value;
+
+      if (calculated) {
+        mainDisplay.textContent = ''
+        calculated = false;
+      }
+      if (mainDisplay.textContent === '0') {
+        mainDisplay.textContent = digit;
+        fontSizeSet();
+      } else {
+        mainDisplay.textContent += digit;
+        fontSizeSet();
+      }
+    }
+
+    if (button.classList.contains('operator')) {
+      if (firstNumber && operator) {
+        secondNumber = +mainDisplay.textContent;
+        mainDisplay.textContent = operate(operator, firstNumber, secondNumber);
+      }
+      firstNumber = +mainDisplay.textContent;
+      operator = button.value;
+      upperDisplay.textContent = `${firstNumber} ${operator}`;
+      mainDisplay.textContent = '';
+    }
+
+    if (button.classList.contains('equals')) {
+      secondNumber = +mainDisplay.textContent;
+      const rawResult = operate(operator, firstNumber, secondNumber);
+      const result = typeof rawResult === 'number' ? parseFloat(rawResult.toFixed(10)) : rawResult;
+      upperDisplay.textContent = `${firstNumber} ${operator} ${secondNumber} = ${result}`;
+      mainDisplay.textContent = result;
+      calculated = true;
+    }
+
+    if (button.classList.contains('clear')) {
+      firstNumber = null;
+      secondNumber = null;
+      operator = null;
+      mainDisplay.textContent = '0'
+      upperDisplay.textContent = ''
+    }
+
+    if (button.classList.contains('backspace')) {
+      mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+    }
+  });
+});
